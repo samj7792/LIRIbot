d4beca51603086047183d3f1b4ac68a6244af095
Sam Jackson <samuelsjackson7@gmail.com>
2019-07-10 21:12:58 -0400

    concert-this works for do-what-it-says, spotify-this-song next

--- liri.js
+++ liri.js
@@ -56,155 +56,167 @@ function doIt() {
 
         console.log(dataArr);
 
-        for (var i=0; i < dataArr.length; i++) {
-            switch (dataArr[0]) {
+        
+        switch (dataArr[0]) {
 
-                case "movie-this":
-            
-                    // Include the axios npm package
-                    var axios = require("axios");
+            case "movie-this":
+        
+                // Include the axios npm package
+                var axios = require("axios");
 
-                    // Grab or assemble the movie name and store it in a variable called "movieName"
-                    var movieName = "";
+                // Grab or assemble the movie name and store it in a variable called "movieName"
+                var movieName = "";
 
-                    for ( var i = 1; i < dataArr.length ; i++) {
-                        movieName += dataArr[i] + "+";
-                    }
+                for ( var i = 1; i < dataArr.length ; i++) {
+                    movieName += dataArr[i] + "+";
+                }
 
-                    // Then run a request with axios to the OMDB API with the movie specified
-                    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+                // Then run a request with axios to the OMDB API with the movie specified
+                var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-                    // This line is just to help us debug against the actual URL.
-                    // console.log(queryUrl);
+                // This line is just to help us debug against the actual URL.
+                // console.log(queryUrl);
 
-                    // Then create a request with axios to the queryUrl
-                    // ...
-                    axios.get(queryUrl).then(
-                        function(response) {
-                            //console.log(response);
-                            var info = response.data;
-                            console.log("Title: " + info.Title);
-                            console.log("Release Year: " + info.Year);
-                            console.log("IMDB Rating: " + info.imdbRating);
-                            console.log("Rotten Tomatoes Score: " + info.Ratings[1].Value);
-                            console.log("Country of Production: " + info.Country);
-                            console.log("Languages: " + info.Language);
-                            console.log("Plot: " + info.Plot);
-                            console.log("Actors: " + info.Actors);
-                        }).catch(function(error) {
-                        if (error.response) {
-                        // The request was made and the server responded with a status code
-                        // that falls out of the range of 2xx
-                        console.log("---------------Data---------------");
-                        console.log(error.response.data);
-                        console.log("---------------Status---------------");
-                        console.log(error.response.status);
-                        console.log("---------------Status---------------");
-                        console.log(error.response.headers);
-                        } else if (error.request) {
-                        // The request was made but no response was received
-                        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                        console.log(error.request);
-                        } else {
-                        // Something happened in setting up the request that triggered an Error
-                        console.log("Error", error.message);
-                        }
-                        console.log(error.config);
-                    });
-            
-                    break;
-                
-                case "spotify-this-song":
-            
-                    var Spotify = require('node-spotify-api');
- 
-                    var spotify = new Spotify(keys.spotify);
-
-                    var songName = "";
-
-                    for ( var i = 1; i < dataArr.length ; i++) {
-                        songName += dataArr[i] + " ";
-                    }
-                    
-                    spotify
-                    .search({ type: 'track', query: songName })
-                    .then(function(response) {
-
-                        // JSON response
-                        // console.log(JSON.stringify(response, null, 2));
-
-                        console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
-                        console.log("Song Name: " + response.tracks.items[0].name);
-                        console.log("Link to Song: " + response.tracks.items[0].external_urls.spotify);
-                        console.log("Album: " + response.tracks.items[0].album.name);
-
-                    })
-                    .catch(function(err) {
-                        console.log(err);
-                    });
-            
-                    break;
-                
-                case "concert-this": 
-            
-                    // Include the axios npm package
-                    var axios = require("axios");
-
-                    var bandsintown = require('bandsintown')("codingbootcamp");
-
-                    var artist = "";
-
-                    for ( var i = 1; i < dataArr.length ; i++) {
-                        artist += dataArr[i] + "%20";
-                    }
-                    console.log(artist);
-
-                    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-                
-                    axios.get(queryUrl).then(
+                // Then create a request with axios to the queryUrl
+                // ...
+                axios.get(queryUrl).then(
                     function(response) {
                         //console.log(response);
-                        
-                        //console.log("Data%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-                        //console.log(response.data);
-
-                        var data = response.data;
-
-                        for (var i = 0 ; i < data.length ; i++){
-                            console.log("----------Venue----------");
-                            console.log(data[i].venue.name);
-                            console.log("----------Location----------");
-                            console.log(data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
-                            console.log("----------Date----------");
-                            console.log(moment(data[i].datetime).format('MMMM Do YYYY, h:mm:ss a'));
-
-                            console.log("\n\n")
-                        }
-                        
-                        }).catch(function(error) {
-                        if (error.response) {
-                        // The request was made and the server responded with a status code
-                        // that falls out of the range of 2xx
-                        console.log("---------------Data---------------");
-                        console.log(error.response.data);
-                        console.log("---------------Status---------------");
-                        console.log(error.response.status);
-                        console.log("---------------Status---------------");
-                        console.log(error.response.headers);
-                        } else if (error.request) {
-                        // The request was made but no response was received
-                        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                        console.log(error.request);
-                        } else {
-                        // Something happened in setting up the request that triggered an Error
-                        console.log("Error", error.message);
-                        }
-                        console.log(error.config);
-                    });
+                        var info = response.data;
+                        console.log("Title: " + info.Title);
+                        console.log("Release Year: " + info.Year);
+                        console.log("IMDB Rating: " + info.imdbRating);
+                        console.log("Rotten Tomatoes Score: " + info.Ratings[1].Value);
+                        console.log("Country of Production: " + info.Country);
+                        console.log("Languages: " + info.Language);
+                        console.log("Plot: " + info.Plot);
+                        console.log("Actors: " + info.Actors);
+                    }).catch(function(error) {
+                    if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                    } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                    } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                });
+        
+                break;
             
-                    break;
-            }
+            case "spotify-this-song":
+        
+                var Spotify = require('node-spotify-api');
+
+                var spotify = new Spotify(keys.spotify);
+
+                var songName = "";
+
+                for ( var i = 1; i < dataArr.length ; i++) {
+                    songName += dataArr[i] + " ";
+                }
+                
+                spotify
+                .search({ type: 'track', query: songName })
+                .then(function(response) {
+
+                    // JSON response
+                    // console.log(JSON.stringify(response, null, 2));
+
+                    console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
+                    console.log("Song Name: " + response.tracks.items[0].name);
+                    console.log("Link to Song: " + response.tracks.items[0].external_urls.spotify);
+                    console.log("Album: " + response.tracks.items[0].album.name);
+
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+        
+                break;
+            
+            case "concert-this": 
+        
+                // Include the axios npm package
+                var axios = require("axios");
+
+                var bandsintown = require('bandsintown')("codingbootcamp");
+
+                var artist = "";
+
+                var artistString = dataArr[1];
+
+                console.log(artistString);
+
+                var fixedString = artistString.replace(/"/g, '');
+
+                console.log(fixedString);
+
+                var artistArr = fixedString.split(" ");
+
+                console.log(artistArr);
+
+                for ( var i = 0; i < artistArr.length ; i++) {
+                    artist += "%20" + artistArr[i];
+                }
+                console.log(artist);
+
+                var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+            
+                axios.get(queryUrl).then(
+                function(response) {
+                    //console.log(response);
+                    
+                    //console.log("Data%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                    //console.log(response.data);
+
+                    var data = response.data;
+
+                    for (var i = 0 ; i < data.length ; i++){
+                        console.log("----------Venue----------");
+                        console.log(data[i].venue.name);
+                        console.log("----------Location----------");
+                        console.log(data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
+                        console.log("----------Date----------");
+                        console.log(moment(data[i].datetime).format('MMMM Do YYYY, h:mm:ss a'));
+
+                        console.log("\n\n")
+                    }
+                    
+                    }).catch(function(error) {
+                    if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                    } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                    } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                });
+        
+                break;
         }
+    
     });
 }
 
@@ -223,6 +235,8 @@ function concert() {
     for ( var i = 3; i < process.argv.length ; i++) {
         artist += "%20" + process.argv[i];
     }
+
+    console.log(artist);
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
  
